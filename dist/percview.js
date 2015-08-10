28c84d03d9bd378c443ce2f63e4192928ae69b77
Jared A. Scheel <jared@jaredscheel.com>
2015-08-10 16:51:28 -0500

    proper dist

--- dist/percview.js
+++ dist/percview.js
@@ -1,158 +1,29 @@
-/*
-https://github.com/jscheel/percview.js
-
-The MIT License (MIT)
-
-Copyright (c) 2015 Jared A. Scheel
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
-(function(window, document, undefined){
-  'use strict';
-
-  var nextFrame = window.requestAnimationFrame ||
-                  window.webkitRequestAnimationFrame ||
-                  window.mozRequestAnimationFrame ||
-                  window.msRequestAnimationFrame ||
-                  window.oRequestAnimationFrame ||
-                  function(callback){ window.setTimeout(callback, 1000/60); };
-
-  var doc, _instance;
-  var percview = {
-    init: function(options) {
-      return _instance || new PercView(options);
-    },
-    get: function() {
-      return _instance;
-    }
-  };
-
-  function PercView(options) {
-    _instance = this;
-    doc = document.documentElement;
-    options = options || {};
-    if (options.elements instanceof Array) {
-      // Ensure it is an array, even if it's really a NodeList
-      _instance.elements = Array.prototype.slice.call(options.elements);
-    }
-    else if (options.elements instanceof Node) {
-      _instance.elements = [options.elements];
-    }
-    else if (typeof options.elements === 'string') {
-      _instance.elements = Array.prototype.slice.call(doc.getElementsByClassName(options.elements));
-    }
-    else {
-      _instance.elements = Array.prototype.slice.call(doc.getElementsByClassName('percview'));
-    }
-    if (typeof options.callback === 'function') {
-      _instance.callback = options.callback;
-    }
-
-    _instance.elementOffsets = {};
-    _instance.calculateOffsets();
-    window.addEventListener('resize', _instance._handleResize);
-    window.addEventListener('scroll', _instance._handleScroll);
-
-    return _instance;
-  }
-
-  PercView.prototype._handleResize = function() {
-    nextFrame(_instance.calculateOffsets);
-  };
-
-  PercView.prototype._handleScroll = function() {
-    nextFrame(_instance.calculatePositions);
-  };
-
-  PercView.prototype.calculateOffsets = function() {
-    for (var i = _instance.elements.length - 1; i >= 0; i--) {
-      _instance.elementOffsets[_instance.elements[i]] = getOffset(_instance.elements[i]);
-    }
-  };
-
-  PercView.prototype.calculatePositions = function() {
-    var retVal = {};
-    for (var i = _instance.elements.length - 1; i >= 0; i--) {
-      retVal[_instance.elements[i]] = _instance.calculatePosition(_instance.elements[i]);
-    }
-    if (_instance.callback) {
-      _instance.callback.apply(_instance, [retVal]);
-    }
-  };
-
-  PercView.prototype.calculatePosition = function(element) {
-    var scrollOffsetY = getScrollOffsetY();
-    var scrollOffsets = {
-      top: scrollOffsetY,
-      bottom: scrollOffsetY + window.innerHeight
-    };
-    var offsets = _instance.elementOffsets[element];
-    // console.log(offsets.top + ',' + offsets.bottom + ',' + scrollOffsets.top + ',' + scrollOffsets.bottom);
-    if ((offsets.top <= scrollOffsets.top && offsets.bottom >= scrollOffsets.bottom) || (offsets.top >= scrollOffsets.top && offsets.bottom <= scrollOffsets.bottom)) {
-      // fully in view
-      return 100;
-    }
-    else if (offsets.top >= scrollOffsets.bottom || offsets.bottom <= scrollOffsets.top) {
-      // out of view
-      return 0;
-    }
-    else if (offsets.top <= scrollOffsets.bottom && offsets.top >= scrollOffsets.top){
-      // partially in view from bottom
-      return ((scrollOffsets.bottom - offsets.top) / offsets.height) * 100;
-    }
-    else if (offsets.top <= scrollOffsets.top && offsets.bottom >= scrollOffsets.top) {
-      // partially in view from top
-      return ((offsets.bottom - scrollOffsets.top) / offsets.height) * 100;
-    }
-    else {
-      console.log('no idea');
-      // no idea
-      return 0;
-    }
-  };
-
-  function getOffset(element) {
-    // This method is modified from the original, which is licensed under the MIT License, Copyright (c) 2010-2015 Google, Inc. http://angularjs.org
-    var box = {top: 0, left: 0};
-    if (typeof element.getBoundingClientRect !== undefined) {
-      box = element.getBoundingClientRect();
-    }
-    var scrollOffsetY = getScrollOffsetY();
-    return {
-      top: box.top + scrollOffsetY,
-      bottom: box.bottom + scrollOffsetY,
-      height: box.height
-    };
-  }
-
-  function getScrollOffsetY() {
-    return (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
-  }
-
-  if(typeof define === 'function' && define.amd) {
-    define([], function () {
-      return percview;
-    });
-  } else if (typeof module !== 'undefined' && module.exports) {
-    module.exports = percview;
-  } else {
-    window.percview = percview;
-  }
-}(window, document));
+/**
+ * https://github.com/jscheel/percview.js
+ *
+ * @license The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Jared A. Scheel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+!function(e,t,o){"use strict";function n(o){return a=this,s=t.documentElement,o=o||{},a.elements=o.elements instanceof Array?Array.prototype.slice.call(o.elements):o.elements instanceof Node?[o.elements]:Array.prototype.slice.call("string"==typeof o.elements?s.getElementsByClassName(o.elements):s.getElementsByClassName("percview")),"function"==typeof o.callback&&(a.callback=o.callback),a.elementOffsets={},a.calculateOffsets(),e.addEventListener("resize",a._handleResize),e.addEventListener("scroll",a._handleScroll),a}function l(e){/**
+     * @license This method is modified from the original, which is licensed under the MIT License, Copyright (c) 2010-2015 Google, Inc. http://angularjs.org
+     */
+var t={top:0,left:0};typeof e.getBoundingClientRect!==o&&(t=e.getBoundingClientRect());var n=i();return{top:t.top+n,bottom:t.bottom+n,height:t.height}}function i(){return(e.pageYOffset||s.scrollTop)-(s.clientTop||0)}var s,a,c=e.requestAnimationFrame||e.webkitRequestAnimationFrame||e.mozRequestAnimationFrame||e.msRequestAnimationFrame||e.oRequestAnimationFrame||function(t){e.setTimeout(t,1e3/60)},r={init:function(e){return a||new n(e)},get:function(){return a}};n.prototype._handleResize=function(){c(a.calculateOffsets)},n.prototype._handleScroll=function(){c(a.calculatePositions)},n.prototype.calculateOffsets=function(){for(var e=a.elements.length-1;e>=0;e--)a.elementOffsets[a.elements[e]]=l(a.elements[e])},n.prototype.calculatePositions=function(){for(var e={},t=a.elements.length-1;t>=0;t--)e[a.elements[t]]=a.calculatePosition(a.elements[t]);a.callback&&a.callback.apply(a,[e])},n.prototype.calculatePosition=function(t){var o=i(),n={top:o,bottom:o+e.innerHeight},l=a.elementOffsets[t];return l.top<=n.top&&l.bottom>=n.bottom||l.top>=n.top&&l.bottom<=n.bottom?100:l.top<n.bottom&&l.bottom>n.top?l.top>n.bottom||l.top<n.top?l.top>n.top||l.bottom<n.top?(console.log("no idea"),0):(l.bottom-n.top)/l.height*100:(n.bottom-l.top)/l.height*100:0},"function"==typeof define&&define.amd?define([],function(){return r}):"undefined"!=typeof module&&module.exports?module.exports=r:e.percview=r}(window,document);
