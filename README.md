665645763d4f8f441820c0aa25ad9b97b9bebf84
Jared A. Scheel <jared@jaredscheel.com>
2015-08-10 16:50:02 -0500

    update readme

--- README.md
+++ README.md
@@ -1,23 +1,41 @@
 # percview.js
-
 This plugin will calculate the visible percentage of elements as the page is scrolled. It it still in
 active development and doesn't really do a good job of reflecting an abstracted case yet. But, I needed
 it for some scrolling behaviors, so this is what you get. Will document once I settle on how this
-plugin actually needs to behave for users.
+plugin actually needs to behave for users. I will also distribute to bower at that point.
 
 ## Initializing
+You can initialize percview.js with one of three element selections:
 
-```
-percview.init({
-  elements: [string | NodeList | Node]
-  callback: [function]
-});
+* class name (without dot)
+* list of elements
+* single element
+
+The callback will be called each time the window is scrolled.
+```html
+  <script src="percview.min.js"></script>
+  <script>
+    var pv = percview.init({
+      elements: [String | NodeList | Node],  // You can pass a class name, list of elements, or single element
+      callback: [function]                   // This function is called when the user scrolls the window
+    });
+
+    // or, if you are using require.js
+
+    require(['percview'], function(percview){
+      var pv = percview.init({
+        elements: [String | NodeList | Node],
+        callback: [function]
+      });
+    });
+  </script>
 ```
 
 ## Callback
+A hash keyed to the elements you are onbserving is returned.
 
-```
-  function(elementsHash) {
-    var percentVisible = elementsHash[element];
-  }
+```javascript
+function(elementsHash) {
+  var percentVisible = elementsHash[element];
+}
 ```
--- src/percview.js
+++ src/percview.js
@@ -1,28 +1,29 @@
-/*
-https://github.com/jscheel/percview.js
+/**
+ * https://github.com/jscheel/percview.js
+ *
+ * @license The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Jared A. Scheel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-The MIT License (MIT)
-
-Copyright (c) 2015 Jared A. Scheel
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 
 (function(window, document, undefined){
   'use strict';
@@ -129,7 +130,9 @@ THE SOFTWARE.
   };
 
   function getOffset(element) {
-    // This method is modified from the original, which is licensed under the MIT License, Copyright (c) 2010-2015 Google, Inc. http://angularjs.org
+    /**
+     * @license This method is modified from the original, which is licensed under the MIT License, Copyright (c) 2010-2015 Google, Inc. http://angularjs.org
+     */
     var box = {top: 0, left: 0};
     if (typeof element.getBoundingClientRect !== undefined) {
       box = element.getBoundingClientRect();
